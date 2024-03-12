e931a5ad5b1340ef3009531af0173076ca8bc612
twicks95 <teguh.wck95@gmail.com>
2024-03-12 10:53:11 +0700

    refactor: Rename component function name card to Card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cardStyles from "./card.module.css";
 
-export default function card({ children, className = "", style }) {
+export default function Card({ children, className = "", style }) {
   return (
     <div style={style} className={`${className.concat(' ', `${cardStyles.container} w-full flex`)}`}>
       {children}
