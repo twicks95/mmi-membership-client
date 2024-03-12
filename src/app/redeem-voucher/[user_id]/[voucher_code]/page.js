a9664b3b82753c2d02497348c160a490186d7f5b
twicks95 <teguh.wck95@gmail.com>
2024-03-12 11:13:40 +0700

    refactor: Set metadata page with react-helmet, Add handling verify voucher, Calling InputCashier component

--- src/app/redeem-voucher/[user_id]/[voucher_code]/page.js
+++ src/app/redeem-voucher/[user_id]/[voucher_code]/page.js
@@ -1,13 +1,19 @@
 "use client"
 
 import Header from "@/components/header/header";
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import QRCode from 'react-qr-code';
-import Card from "../../../../components/card/card.jsx"
-import Button from "@/components/button/button.jsx"
+import Card from "../../../../components/card/Card.jsx"
+import Button from "@/components/button/Button.jsx"
+import InputCashier from "@/components/InputCashier/InputCashier.jsx";
+import { Helmet } from "react-helmet";
 
 export default function RedeemVoucher({ params }) {
+    const pageTitle = 'Redeem Voucher | Membership';
+    const pageDescription = 'A voucher redeemed QR code';
+
     const [userData, setUserData] = useState({})
+    const inputCashierRef = useRef()
 
     const fetchUser = async ({ userId }) => {
         await fetch(`http://localhost:3001/api/user/${userId}`, {
@@ -32,33 +38,46 @@ export default function RedeemVoucher({ params }) {
         fetchUser({ userId: params.user_id })
     }, [params])
 
+
+    const handleVerify = () => {
+        inputCashierRef.current.open()
+    }
+
     return (
-        <div className="flex items-center py-[18%]">
-            <Header title="Redeem Voucher" userData={userData} />
-            <Card className="justify-center items-center !py-16">
-                <div className="flex flex-col justify-center items-center h-full">
-                    <div className="text-lg font-bold mb-7">MMI Membership</div>
-                    <div className="flex flex-col items-center mb-4">
-                        <div className="font-bold">Your QR Code is ready</div>
-                        <div className="text-sm">Get it scanned to apply in your transaction</div>
+        <>
+            <Helmet>
+                <title>{pageTitle}</title>
+                <meta name="description" content={pageDescription} />
+            </Helmet>
+            <div className="flex items-center py-[18%]">
+                <Header title="Redeem Voucher" userData={userData} />
+                <Card className="flex-col justify-center items-center !py-16">
+                    <div className="flex flex-col justify-center items-center h-full mb-6">
+                        <div className="text-lg font-bold mb-7">MMI Membership</div>
+                        <div className="flex flex-col items-center mb-4">
+                            <div className="font-bold">Your QR Code is ready</div>
+                            <div className="text-sm">Get it scanned to apply in your transaction</div>
+                        </div>
+
+                        {/* Customize size and error correction (optional) */}
+                        <div className="mb-5 border-4 border-solid border-[#0A24AD] p-2 rounded-[10px]">
+                            <QRCode value={`/redeem/voucher_code/${params.voucher_code}`} size={226} level="L" bgColor="#fff" />
+                        </div>
+                        <div className="font-medium">----- Voucher Code -----</div>
+                        <div className="font-bold">{params.voucher_code}</div>
                     </div>
 
-                    {/* Customize size and error correction (optional) */}
-                    <div className="mb-5 border-4 border-solid border-[#0A24AD] p-2 rounded-[10px]">
-                        <QRCode value={`/redeem/voucher_code/${params.voucher_code}`} size={226} level="L" bgColor="#fff" />
-                    </div>
-                    <div className="font-medium">----- Voucher Code -----</div>
-                    <div className="font-bold">{params.voucher_code}</div>
-                </div>
+                    <Button
+                        value="verify"
+                        text="Verify"
+                        className="h-[38px] !w-[63%] rounded-[10px] bg-[#0A24AD] hover:bg-[#3A4FBC] text-sm text-white hover:!text-white"
+                        onClick={handleVerify}
+                    />
 
-                <Button
-                    text="Apply filter"
-                    htmlType="submit"
-                    // loading={confirmLoading}
-                    className="h-[38px] rounded-[10px] bg-[#0A24AD] hover:bg-[#3A4FBC] text-sm text-white hover:!text-white"
-                    // onClick={handleApplyFilter}
-                />
-            </Card>
-        </div>
+                    {/* Drawer */}
+                    <InputCashier passedInRef={inputCashierRef} />
+                </Card>
+            </div >
+        </>
     )
 }
